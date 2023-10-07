2eeabd294e938a930cd82511b87497d767e3cb38
Anand Mathew Sabu <anandmathew29@gmail.com>
2023-10-07 15:25:48 -0400

    Update and rename ItemAddedtoCart.java to ItemAddtoCartPage.java

--- ItemAddtoCartPage.java
+++ ItemAddtoCartPage.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class ItemAddedtoCart {
+public class ItemAddtoCartPage {
 	
 	public void addToWishlist(WebDriver driver){
 		

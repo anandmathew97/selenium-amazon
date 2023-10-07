8a34389d7f97ea24477a29dd5d545a1cf549bc53
Anand Mathew Sabu <anandmathew29@gmail.com>
2023-10-07 15:18:33 -0400

    Update and rename ItemPage.java to ItemListingPage.java

--- ItemListingPage.java
+++ ItemListingPage.java
@@ -1,11 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class ItemPage {
+public class ItemListingPage {
 	
 	public void clickItem(WebDriver driver) {
 		
-        driver.findElement(By.xpath("//*[text()='Apple Iphone 12 Mini']")).click();
+        driver.findElement(By.xpath("//*[text()='SAMSUNG Galaxy S23']")).click();
         
 	}
     

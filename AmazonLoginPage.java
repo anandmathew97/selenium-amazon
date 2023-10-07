2acd1de6046119fea9f231fcc456dd3756abd1bb
Anand Mathew Sabu <anandmathew29@gmail.com>
2023-10-07 14:34:43 -0400

    Create AmazonLoginPage.java

---
+++ AmazonLoginPage.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AmazonLoginPage {
+	
+	public void logInTheForm(WebDriver driver) {
+		
+		String email = "youremail@provider.com";
+		String pw = "yourpw";
+		
+        	driver.findElement(By.id("ap_email")).sendKeys(email);  
+          driver.findElement(By.id("Continue")).click();
+        	driver.findElement(By.id("ap_password")).sendKeys(pw);       
+        	driver.findElement(By.id("signInSubmit")).click();
+        
+	}
+    
+}
